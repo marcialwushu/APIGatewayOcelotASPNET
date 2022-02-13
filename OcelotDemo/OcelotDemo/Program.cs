e0cd2738e261b2ce313b292e139e65c8ed7db0fe
cleilson pereira <marcialwushu@hotmail.com>
2022-02-13 18:47:57 -0300

    :fire: Ocelot json config add

--- OcelotDemo/OcelotDemo/Program.cs
+++ OcelotDemo/OcelotDemo/Program.cs
@@ -20,7 +20,10 @@ namespace OcelotDemo
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.ConfigureAppConfiguration(config =>
+                        config.AddJsonFile($"ocelot.{env}.json"));
                 })
             .ConfigureLogging(logging => logging.AddConsole());
     }
